cbb17f7b274f0a4ad5475dd8b2329581025138ed
Eduardo Souza <eduardomatosouza@gmail.com>
2014-01-18 14:03:50 -0200

    Add spinner style. Fix amount error message.

--- src/main/java/br/com/eduardo/loan/util/validator/ItemValidator.java
+++ src/main/java/br/com/eduardo/loan/util/validator/ItemValidator.java
@@ -4,11 +4,14 @@ import android.content.Context;
 import android.widget.Toast;
 import br.com.eduardo.loan.R;
 import br.com.eduardo.loan.model.entity.ItemDTO;
+import br.com.eduardo.loan.util.type.ItemFormatter;
 
 /**
  * @author Eduardo Matos de Souza<br>
  *         04/06/2011 <br>
- *         <a href="mailto:eduardomatosouza@gmail.com">eduardomatosouza@gmail.com</a>
+ *         <a
+ *         href="mailto:eduardomatosouza@gmail.com">eduardomatosouza@gmail.com
+ *         </a>
  */
 public class ItemValidator {
 
@@ -16,8 +19,17 @@ public class ItemValidator {
         if (item.getTitle() != null && item.getTitle().length() > 0) {
             return true;
         } else {
-            Toast toast = Toast.makeText(context, R.string.msg_item_name_error, Toast.LENGTH_SHORT);
-            toast.show();
+            if (item.getType() == ItemFormatter.TYPE_MONEY) {
+                Toast toast = Toast.makeText(context,
+                        String.format(context.getString(R.string.msg_item_name_error), context.getString(R.string.label_amount)).toLowerCase(),
+                        Toast.LENGTH_SHORT);
+                toast.show();
+            } else {
+                Toast toast = Toast.makeText(context,
+                        String.format(context.getString(R.string.msg_item_name_error), context.getString(R.string.label_title)).toLowerCase(),
+                        Toast.LENGTH_SHORT);
+                toast.show();
+            }
             return false;
         }
     }
